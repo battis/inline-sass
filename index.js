40c7d8d828bff4277f8768b9e08d94bdd261c034
Seth Battis <seth@battis.net>
2021-10-24 18:01:50 +0000

    CSS in tmp
    Rather than inlining the CSS, generate CSS files in the OS temp directory (cleaning up at the end of the run).

--- index.js
+++ index.js
@@ -1,68 +1,67 @@
-import fs from "fs";
-import path from "path";
-import { parse } from "node-html-parser";
-import sass from "sass";
-import inlineCSS from "inline-css";
-
-const defaults = {
-	devEltSelector: '[data-purpose="dev"]'
-};
+import os from 'os';
+import fs from 'fs';
+import path from 'path';
+import { parse } from 'node-html-parser';
+import sass from 'sass';
+import inlineCSS from 'inline-css';
 
 const inlineSass = async (path_or_html, options) => {
-	
-	options = {...defaults, ...options};
-	
-	const removeDevElements = (dom, selector) => {
-		for(const elt of dom.querySelectorAll(selector)) {
-			elt.remove();
-		}
-		return dom;
-	}
-	
-	const embedTranspiledLinkedSass = (dom, base) => {
-		const links = dom.querySelectorAll('link[rel="stylesheet"][href$="css"]');
-		for(const link of links) {
-			let file = link._attrs.href;
-			if (base) {
-				file = path.join(base, file);
-			}
-			const css = sass.renderSync({file}).css;
-			dom.insertAdjacentHTML('afterbegin', '<style>\n' + String(css) + '\n</style>');
-			link.remove();
-		}
-		return dom;
-	};
-	
-	let html = path_or_html;
-	let dir = undefined;
-	if (fs.existsSync(path_or_html)) {
-		html = fs.readFileSync(path_or_html);	
-		dir = path.dirname(path_or_html);	
-	}
-	let dom = parse(html);
-	
-	if (!dom) {
-		if (isPath) {
-			throw 'file "' + path_or_html + '" unparseable';
-		} else {
-			throw '"' + path_or_html + '" not HTML';
-		}
-	}
-	
-	if (options.devEltSelector) {
-		dom = removeDevElements(dom, options.devEltSelector);
-	}
-	
-	dom = embedTranspiledLinkedSass(dom, dir);
-	
-	
-	let result = undefined;
-	try {
-		result = await inlineCSS(String(dom), {url: dir, ...options});
-	} catch (e) {
-		console.log(e);
-	}
-	return result;
-}
+    let html = path_or_html;
+    let dir =
+        options.url.search(/^file:\/\//) === 0
+            ? options.url.replace(/^file:\/\//, '')
+            : undefined;
+    const tmp = fs.mkdtempSync(path.join(os.tmpdir(), 'css-'));
+
+    const transpileSass = (dom, base) => {
+        const links = dom.querySelectorAll(
+            'link[href$=".sass"],link[href$=".scss"]'
+        );
+        for (const link of links) {
+            const href = link.getAttribute('href');
+            let sassFile = href;
+            if (base) {
+                sassFile = path.join(base, sassFile);
+            }
+            let cssFile = path.join(
+                tmp,
+                path.basename(path.basename(href, '.sass'), '.scss') + '.css'
+            );
+            fs.writeFileSync(cssFile, sass.renderSync({ file: sassFile }).css);
+            link.setAttribute('href', `file://${cssFile}`);
+            link.setAttribute('data-original-href', href);
+        }
+        return dom;
+    };
+
+    if (fs.existsSync(path_or_html)) {
+        html = fs.readFileSync(path_or_html);
+        dir = path.dirname(path_or_html);
+    }
+    let dom = parse(html);
+
+    if (!dom) {
+        if (dir) {
+            throw 'file "' + path_or_html + '" unparseable';
+        } else {
+            throw '"' + path_or_html + '" not HTML';
+        }
+    }
+
+    dom = transpileSass(dom, dir || options.url.replace(/(^\w+:\/\/)/, ''));
+
+    let result = undefined;
+    try {
+        result = await inlineCSS(String(dom), {
+            url: `file://${dir}/`,
+            ...options
+        });
+    } catch (e) {
+        console.log(e);
+    }
+
+    fs.rmdirSync(tmp, { recursive: true });
+    return result;
+};
 
 export default inlineSass;
